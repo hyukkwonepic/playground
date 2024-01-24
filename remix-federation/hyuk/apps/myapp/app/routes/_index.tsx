550d054894be78f9a4fef58778ee8c62ade59186
Hyo Kwon <hyo.kwon@outlook.com>
2024-01-24 13:18:39 +0900

    add login package and use it on myapp

--- remix-federation/hyuk/apps/myapp/app/routes/_index.tsx
+++ remix-federation/hyuk/apps/myapp/app/routes/_index.tsx
@@ -1,3 +1,5 @@
+import { Login } from '@hyuk/libs/login';
+
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
@@ -27,6 +29,7 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <Login />
     </div>
   );
 }
