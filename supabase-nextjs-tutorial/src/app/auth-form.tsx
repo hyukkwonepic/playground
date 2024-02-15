a4a0f3ebf0cdfffd9519d9c06ea595083c96cbf4
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-02-15 23:42:48 +0900

    Add example from https://supabase.com/docs/guides/getting-started/tutorials/with-nextjs

---
+++ supabase-nextjs-tutorial/src/app/auth-form.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import React from "react";
+import { Database } from "./supabase";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+function AuthForm({}: Props) {
+  const supabase = createClientComponentClient<Database>();
+  return (
+    <Auth
+      supabaseClient={supabase}
+      view="magic_link"
+      appearance={{ theme: ThemeSupa }}
+      theme="dark"
+      showLinks={false}
+      providers={[]}
+      redirectTo="http://localhost:3000/auth/callback"
+    />
+  );
+}
+
+export default AuthForm;
---
+++ supabase-nextjs-tutorial/src/middleware.ts
@@ -0,0 +1,27 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log("user", user);
+
+  if (user && req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/account", req.url));
+  }
+
+  if (!user && req.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/", "/account"],
+};
